b5a84f12d882b6c226761ac8db6deabba841f458
Henrique Costa <rafahsc@live.com>
2025-10-06 14:37:00 -0300

    Criado projeto back-end com quarkus

---
+++ parking-m/src/test/java/br/com/rhscdeveloper/ApplicationIT.java
@@ -0,0 +1,8 @@
+package br.com.rhscdeveloper;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ApplicationIT extends ApplicationTest {
+    // Execute the same tests but in packaged mode.
+}
